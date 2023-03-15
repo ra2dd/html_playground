79f3152f8af5fcf47209266fa1e2240fdd8accaa
ra2dd <98891234+ra2dd@users.noreply.github.com>
2023-03-15 12:50:41 +0100

    added images and dome js

--- main.js
+++ main.js
@@ -1,46 +1,13 @@
-const output = document.querySelector("#output");
-const alarmButton = document.querySelector("#set-alarm");
-const name = document.querySelector("#alarm-name");
-const delay = document.querySelector("#alarm-delay");
+const img = document.querySelector('img');
 
-
-/*
-function setAlarm()
+img.addEventListener('click', () => 
 {
-    setTimeout(() => 
+    if(img.getAttribute('src') === 'firefox-icon.jpg')
     {
-        output.textContent += "\nWake up!";
-    }, 1000);
-}
-
-alarmButton.addEventListener('click', setAlarm);
-
-*/
-
-function alarm(person, delay)
-{
-    return new Promise((resolve, reject) => 
-    {
-        if(delay < 0)
-        {
-            throw new Error('Alarm delay must be positive');
-        }
-        setTimeout( () =>
-        {
-            resolve(`Wake up, ${person}!`);
-        }, delay);
-    });
-}
-
-alarmButton.addEventListener('click', async () =>
-{
-    try
-    {
-        const message = await alarm(name.value, delay.value);
-        output.textContent = message;
+        img.setAttribute('src', 'firefox.jpg');
     }
-    catch (error)
+    else
     {
-        output.textContent = `Couldn't set alarm ${error}`;
+        img.setAttribute('src', 'firefox-icon.jpg');
     }
 });
